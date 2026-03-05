4f217c5d757d8a776ce7b094a1163e1f8c4a8b21
theflysong <song_of_the_fly@163.com>
2026-03-05 15:44:45 +0800

    fix compile error

--- include/std/c++/features/sus/errors.h
+++ include/std/c++/features/sus/errors.h
@@ -12,16 +12,19 @@
 #pragma once
 
 #ifdef __SUS_NO_EXCEPTIONS__
-// sustcore no exception std
-// #define SUSNE_STD
 
-// #define SUS_EXCEPTION_DEPRECATED                                                                  \
-//     [[deprecated(                                                                                 \
-//         "此函数不应被调用!其使用了异常机制, "                                     \
-//         "但内核中不应使用该机制!请调用其对应的无异常版本(一般命名为 " \
-//         "sus_xxx)!")]]
+/*
+sustcore no exception std
+#define SUSNE_STD
+
+#define SUS_EXCEPTION_DEPRECATED                                                                  \
+    [[deprecated(                                                                                 \
+        "此函数不应被调用!其使用了异常机制, "                                     \
+        "但内核中不应使用该机制!请调用其对应的无异常版本(一般命名为 " \
+        "sus_xxx)!")]]
 
 #define SUS_EXCEPTION_DEPRECATED
+*/
 
 enum class STDErrorCode {
     Success         = 0,
@@ -33,11 +36,5 @@ enum class STDErrorCode {
     OutOfRange      = 6,
     BadAlloc        = 7,
 };
-
-#include <sus/optional.h>
-
-template <typename T>
-using STDResult = util::Optional<T, STDErrorCode, STDErrorCode::Success,
-                                    STDErrorCode::UnknownError>;
 
 #endif
